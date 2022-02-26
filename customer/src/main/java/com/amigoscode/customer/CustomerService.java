c6191a31ab1da0fecbaefc65edd47486122db845
David <dgpeek89@gmail.com>
2022-02-26 14:34:32 +0100

    Initial commit

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,31 @@
+package com.amigoscode.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse response = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+        if(response.isFraudster()){
+            throw new IllegalStateException("criminal detected.");
+        }
+
+    }
+}
